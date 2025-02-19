662d1cc4da0f49a4caa5a7fe5b75632b95d46630
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-02-19 18:33:15 +0100

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,8 @@
-"use client";
-
 import Image from "next/image";
-import { signIn, signOut } from "@/auth";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        action={signIn}
-      >
-        Login
-      </button>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        action={signOut}
-      >
-        Logout
-      </button>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -29,13 +14,13 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Empieza editando{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.js
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Guarda y ve tus cambios al instante.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -52,7 +37,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Implementar ahora
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
@@ -60,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Lee nuestra documentación
           </a>
         </div>
       </main>
@@ -78,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Aprender
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -86,14 +71,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          Ejemplos
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -101,16 +79,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Ir a nextjs.org →
         </a>
       </footer>
     </div>
   );
 }
+
